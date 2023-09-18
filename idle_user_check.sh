3b9ec1446bf034897e9489c15a4cd75d311d7cda
Thet Han Kyaw <thethan_kyaw@singlife.com>
2023-09-18 11:11:46 +0800

    update idle_user_check

--- idle_user_check.sh
+++ idle_user_check.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 > users_data.csv
 echo "UserName, Latest Login Date, Cron Status" >> users_data.csv
 
@@ -8,7 +7,7 @@ for user in $(awk -F: '$3 >= 1000 {print $1}' /etc/passwd); do
     latest_login_date=$(last -n 1 -F $user | awk 'NR==1,OFS="-" {print $8,$5,$6}')
     
     
-    if crontab -l -u "$user" 2>/dev/null | grep -q '[0-9]'; then
+    if crontab -l -u "$user" 2>/dev/null | grep -v '^#' | grep -q '[0-9]\|*'; then
         cron_status="true"
     else
         cron_status="false"
@@ -16,6 +15,5 @@ for user in $(awk -F: '$3 >= 1000 {print $1}' /etc/passwd); do
     
     echo "$user, $latest_login_date, $cron_status" >> users_data.csv
 done
-
 
 echo "User data has been saved to users_data.csv"
